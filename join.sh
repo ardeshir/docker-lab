63725bfff3506d0a9a0c463ab891d59a9d40f4d8
ardeshir sepahsalar <ardeshir.org@gmail.com>
2017-01-17 14:23:53 -0600

    added join

---
+++ join.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TOKEN=$(docker swarm join-token -q worker)
+
+for i in 2 3; do
+  eval $(docker-machine env node-$i)
+
+  docker swarm join \
+  --token $TOKEN   \
+  --advertise-addr $(docker-machine ip node-$i) \
+ $(docker-machine ip node-1):2377
+
+done
--- run.sh
+++ run.sh
@@ -2,15 +2,14 @@
 
 TOKEN= $(docker swarm join-token -q worker)
 
-function creat3() {
 for i in 1 2 3; do
   docker-machine create -d virtualbox node-$i
 done
-}
+
 
 for i in 2 3; do
   
-  eval ($docker-machine env node-1)
+  eval ($docker-machine env node-$i)
 
   docker swarm join \
   --token $TOKEN    \
